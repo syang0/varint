68f2c5213cf5f9370c7027d054e8ef530621ed5b
Stephen Yang <syang0@stanford.edu>
2019-06-23 19:37:25 -0700

    Changed output to be in a tabular format

--- varint.cpp
+++ varint.cpp
@@ -25,17 +25,23 @@
 
 using namespace std;
 
-// Generate n log-uniform random numbers.
-vector<uint64_t> gen_log_uniform() {
+/**
+ * Generate n log-uniform random numbers with maximum value
+ * equal to 2^maxBits
+ *
+ * \param maxBits
+ *      restricts the maximum value to the second power of this number
+ * \param minBits
+ *      restrics the minimum value to the second power of this number
+ *
+ */
+vector<uint64_t> gen_log_uniform(int minBits = 0, int maxBits=64) {
 
   // How many uint64_t numbers in the vector we're compressing.
   const size_t N = 10000000;
 
-  // Maximum significant bits in the log-uniform random numbers.
-  const unsigned random_bits = 64;
-
   default_random_engine gen;
-  uniform_real_distribution<double> dist(0.0, random_bits * log(2));
+  uniform_real_distribution<double> dist(minBits * log(2), maxBits * log(2));
   vector<uint64_t> vec;
 
   vec.reserve(N);
@@ -43,6 +49,22 @@ vector<uint64_t> gen_log_uniform() {
     vec.push_back(exp(dist(gen)));
   return vec;
 }
+
+// Stores the benchmark result of a single algorithm for a single run
+struct TestResult {
+  const char *algorithmName;
+  double encodeSecs;
+  double decodeSecs;
+  uint64_t inputBytes;
+  uint64_t outputBytes;
+};
+
+// Stores the result of multiple algorithms given a single input dataset
+struct TestSuite {
+  int minBits;
+  int maxBits;
+  std::vector<TestResult> results;
+};
 
 // Read test vector from a file.
 //
@@ -83,11 +105,12 @@ double time_decode(const uint8_t *in, vector<uint64_t> &out, decoder_func func,
   return secs/1.0e9;
 }
 
-double do_codec(const codec_descriptor &codec,
+TestResult do_codec(const codec_descriptor &codec,
                 const vector<uint64_t> &numbers,
                 uint8_t *scratch)
 {
   vector<uint64_t> decompressed(numbers.size());
+  codec.encoder(numbers, scratch); // Warmup decode
 
   using namespace chrono;
   auto before = high_resolution_clock::now();
@@ -99,13 +122,33 @@ double do_codec(const codec_descriptor &codec,
   double dtime = time_decode(scratch, decompressed, codec.decoder);
   assert(decompressed == numbers);
 
-  printf("%-15s: %10.2f %10.02f %10.2f\r\n",
-            codec.name,
-            double(encodedSize) / numbers.size(),
-            (sizeof(uint64_t)*numbers.size()/encode_secs)/1.0e6,
-            (encodedSize/(dtime))/1.0e6);
+  // printf("%-15s: %10.2f %10.02f %10.2f\r\n",
+  //           codec.name,
+  //           double(encodedSize) / numbers.size(),
+  //           (sizeof(uint64_t)*numbers.size()/encode_secs)/1.0e6,
+  //           (encodedSize/(dtime))/1.0e6);
 
-  return dtime;
+  return {codec.name, encode_secs, dtime, sizeof(numbers.front())*numbers.size(), encodedSize};
+}
+
+void printResults(std::vector<TestSuite> tests) {
+  printf("\r\n# Compressing 64-bit numbers with uniform random bits between n and m\r\n");
+  printf("# Each column represent \"speed (MB/s)\"\r\n");
+  printf("#%-15s", "Algo / bits");
+  for (TestSuite tr : tests)
+    printf("  %2d - %2d", tr.minBits, tr.maxBits);
+  printf("\r\n");
+
+
+  for (int j = 0; j < tests.front().results.size(); ++j) {
+
+    printf("%-16s", tests.front().results[j].algorithmName);
+    for (int i = 0; i < tests.size(); ++i) {
+      TestResult results = tests[i].results[j];
+      printf(" %8.2f", results.inputBytes/(1.0e6*results.encodeSecs));
+    }
+    printf("\r\n");
+  }
 }
 
 int main(int argc, const char *argv[]) {
@@ -114,10 +157,10 @@ int main(int argc, const char *argv[]) {
 
   if (argc == 2) {
     numbers = read_test_vector(argv[1]);
-    printf("Read %zu integers from %s.\n", numbers.size(), argv[1]);
+    printf("#Read %zu integers from %s.\n", numbers.size(), argv[1]);
   } else {
     numbers = gen_log_uniform();
-    printf("Generated %zu log-uniform integers.\n", numbers.size());
+    printf("#Generated %zu log-uniform integers.\n", numbers.size());
   }
 
   // Allocate an output buffer 2x the size of our input numbers
@@ -128,13 +171,56 @@ int main(int argc, const char *argv[]) {
     exit(1);
   }
 
-  printf("#%-14s %10s %10s %10s\r\n", "Algorithm", "bytes/int", "Encode MB/s", "Decode MB/s");
-  do_codec(leb128_codec, numbers, scratch);
-  double leb128 = do_codec(leb128_codec, numbers, scratch);
-  double prefix = do_codec(prefix_codec, numbers, scratch);
-  double sqlite = do_codec(lesqlite_codec, numbers, scratch);
-  double sqlite2 = do_codec(lesqlite2_codec, numbers, scratch);
-  double nanolog = do_codec(nanolog_codec, numbers, scratch);
+  std::vector<TestSuite> randomUpTo;
+  for (int i = 8; i <= 64; i += 8) {
+    // printf("\r\n# With uniform random number of significant bits up to %d\r\n", i);
+    // printf("#%-14s %10s %10s %10s\r\n", "Algorithm", "bytes/int", "Encode MB/s", "Decode MB/s");
+
+    numbers = gen_log_uniform(0, i);
+    std::vector<TestResult> randomRun;
+    randomRun.push_back(do_codec(prefix_codec, numbers, scratch));
+    randomRun.push_back(do_codec(leb128_codec, numbers, scratch));
+    randomRun.push_back(do_codec(lesqlite2_codec, numbers, scratch));
+    randomRun.push_back(do_codec(lesqlite_codec, numbers, scratch));
+    randomRun.push_back(do_codec(nanolog_codec, numbers, scratch));
+    randomUpTo.push_back({0, i,randomRun});
+  }
+
+  std::vector<TestSuite> randomBetween;
+  for (int i = 8; i <= 64; i += 8) {
+    // printf("\r\n# With uniform random number of significant between %d and %d\r\n", i-8, i);
+    // printf("#%-14s %10s %10s %10s\r\n", "Algorithm", "bytes/int", "Encode MB/s", "Decode MB/s");
+
+    numbers = gen_log_uniform(i-8, i);
+    std::vector<TestResult> randomRun;
+    randomRun.push_back(do_codec(prefix_codec, numbers, scratch));
+    randomRun.push_back(do_codec(leb128_codec, numbers, scratch));
+    randomRun.push_back(do_codec(lesqlite2_codec, numbers, scratch));
+    randomRun.push_back(do_codec(lesqlite_codec, numbers, scratch));
+    randomRun.push_back(do_codec(nanolog_codec, numbers, scratch));
+
+    randomBetween.push_back({i-8, i, randomRun});
+  }
+
+  // Print Results.
+  printResults(randomUpTo);
+  printResults(randomBetween);
+
+  // printf("\r\n# Compressing 64-bit numbers with uniform random bits between n and m\r\n");
+  // printf("# Each column represent \"speed (MB/s)\"\r\n");
+  // printf("#%-6s", "Bits");
+  // for (TestResult tr : randomBetween.front().results)
+  //   printf(" %10s", tr.algorithmName);
+  // printf("\r\n");
+
+  // for (TestSuite ts : randomBetween) {
+  //   printf("%2d - %2d", ts.minBits, ts.maxBits);
+  //   for (TestResult tr: ts.results)
+  //     printf(" %10.2f", (tr.inputBytes/tr.encodeSecs)/1.0e6);
+
+  //   printf("\r\n");
+  // }
+
 
   // printf("T(LEB128) / T(PrefixVarint) = %.3f.\n", leb128 / prefix);
   // printf("T(LEB128) / T(leSQLite) = %.3f.\n", leb128 / sqlite);
